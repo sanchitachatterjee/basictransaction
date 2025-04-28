efedf057a8a48e1dd01219e02984c5f046571e08
Sanchita Chatterjee <sanchitachatterjee42@example.com>
2025-04-28 18:25:11 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+      turbopack: false,
+    },
+    images: {
+      domains: ['your-image-host.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
